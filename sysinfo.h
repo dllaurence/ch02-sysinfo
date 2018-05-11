07eddeb43ba9990aca9d36bf2732128ed0c72fe8
Dustin Laurence <dustin@laurences.net>
2018-05-11 13:55:29 -0700

    Make SysInfo a singleton.

--- sysinfo.h
+++ sysinfo.h
@@ -6,13 +6,20 @@ class SysInfo
 {
 public:
 
-    SysInfo();
+    static SysInfo& instance();
     virtual ~SysInfo();
+
+    SysInfo(const SysInfo& rhs) = delete;
+    SysInfo& operator=(const SysInfo& rhs) = delete;
 
     virtual void init() = 0;
 
     virtual double cpuLoadAverage() = 0;
     virtual double memoryUsed() = 0;
+
+protected:
+
+    explicit SysInfo();
 };
 
 #endif // SYSINFO_H
